43d05b319f924dc55636ca19fa83dce19d493938
William Hitchcock <willh@getambassador.com>
2017-09-19 19:59:07 -0400

    - Let's just export the grid components for now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import col from './grid/col'
 import container from './grid/container'
 import row from './grid/row'
-import pseudo from './const/pseudo'
-import sizes from './const/sizes'
-import flex from './util/flex'
-import media from './util/media'
 
 export { col, container, row, pseudo, sizes, flex, media }
