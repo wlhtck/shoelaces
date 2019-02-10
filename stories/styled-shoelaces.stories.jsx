167250d165a01ab8399ff7437c680060b93d94fa
William Hitchcock <hi@willhitchcock.me>
2019-02-10 11:57:21 -0500

    Do some cleanup, emotion example working in storybook

--- stories/styled-shoelaces.stories.jsx
+++ stories/styled-shoelaces.stories.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-import { Col, Container, Row } from '../packages/styled-shoelaces/src';
+import { Col, Container, Row } from '../packages/styled-shoelaces/lib';
 import Box from '../common/Box';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
 
 storiesOf('💅StyledShoelaces', module)
   .add('Basic Example', () => (
